b90f7ebff15dae1bc0d24231250284f82c7e2159
sunset <chieubkhn@gmail.com>
2020-08-15 05:35:40 +0700

    binding Ischeck

--- WPF_MVVM/control custom/SwitchTypeToggleButtonSample/MainWindow.xaml.cs
+++ WPF_MVVM/control custom/SwitchTypeToggleButtonSample/MainWindow.xaml.cs
@@ -1,15 +1,45 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 
 namespace SwitchTypeToggleButtonSample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool _isCheck;
+
+        public bool IsCheck
+        {
+            get { return _isCheck; }
+            set
+            {
+                _isCheck = value;
+                OnPropertyChanged("IsCheck");
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            IsCheck = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string newName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(newName));
+            }
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsCheck = !IsCheck;
         }
     }
 }
