81a673be36cf71caa42553167110263a27fde6c4
sunset2410 <chieubkhn@gmail.com>
2021-11-27 10:10:22 +0700

    Queue

---
+++ other sample/QueueString/QueueString/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QueueString
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Queue queue = new Queue();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            queue.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            queue.insertPIN("a1");
+            queue.insertPIN("a2");
+            queue.insertPIN("a3");
+            queue.insertPIN("a4");
+            queue.insertPIN("a5");
+            queue.insertPIN("a6");
+            queue.insertPIN("a7");
+
+            bool check = queue.ContainsPIN("a6");
+            check = queue.ContainsPIN("a20");
+            check = queue.ContainsPBA("a2");
+
+
+        }
+    }
+}
---
+++ other sample/QueueString/QueueString/Queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueString
+{
+
+    public class Queue
+    {
+        private const int MAX_QUEUE = 5;
+        private Queue<string> PBAQueue;
+        private Queue<string> PINQueue;
+
+        public Queue()
+        {
+            PBAQueue = new Queue<string>();
+            PINQueue = new Queue<string>();
+        }
+
+        public void Clear()
+        {
+            PBAQueue.Clear();
+            PINQueue.Clear();
+        }
+
+        public void insertPIN(string PIN)
+        {
+            if (PINQueue.Count >= MAX_QUEUE)
+            {
+                PINQueue.Dequeue();
+            }
+            PINQueue.Enqueue(PIN);
+        }
+
+        public void insertPBA(string PBA)
+        {
+            if (PBAQueue.Count >= MAX_QUEUE)
+            {
+                PBAQueue.Dequeue();
+            }
+            PBAQueue.Enqueue(PBA);
+        }
+
+        public bool ContainsPIN(string PIN)
+        {
+            if (PINQueue.Contains(PIN))
+                return true;
+            else
+                return false;
+        }
+
+        public bool ContainsPBA(string PBA)
+        {
+            if (PBAQueue.Contains(PBA))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
